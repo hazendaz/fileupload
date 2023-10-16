b83782ec4ca1cd816d4c8dda0c6b782018a45054
Jeremy Landis <jeremylandis@hotmail.com>
2023-10-15 20:15:39 -0400

    [ci] formatting

--- src/main/java/com/hazendaz/weld/BeanProvider.java
+++ src/main/java/com/hazendaz/weld/BeanProvider.java
@@ -41,7 +41,6 @@ import org.apache.deltaspike.core.util.metadata.builder.AnnotatedTypeBuilder;
  * because the current class is not managed by the CDI Container. This can happen in e.g. a JPA-2.0 EntityListener, a
  * ServletFilter, a Spring managed Bean, etc.
  * </p>
- *
  * <p>
  * <b>Attention:</b> This method is intended for being used in user code at runtime. If this method gets used during
  * Container boot (in an Extension), non-portable behavior results. The CDI specification only allows injection of the
@@ -54,9 +53,8 @@ import org.apache.deltaspike.core.util.metadata.builder.AnnotatedTypeBuilder;
 public final class BeanProvider {
 
     /**
-     * Allows to perform dependency injection for instances which aren't managed by CDI.
-     *
-     * Attention: The resulting instance isn't managed by CDI; only fields annotated with @Inject get initialized.
+     * Allows to perform dependency injection for instances which aren't managed by CDI. Attention: The resulting
+     * instance isn't managed by CDI; only fields annotated with @Inject get initialized.
      *
      * @param instance
      *            current instance
@@ -64,6 +62,7 @@ public final class BeanProvider {
      *            current type
      * @param ignoreMap
      *            map of properties to ignore
+     *
      * @return instance with injected fields (if possible - or null if the given instance is null)
      */
     @SuppressWarnings("unchecked")
