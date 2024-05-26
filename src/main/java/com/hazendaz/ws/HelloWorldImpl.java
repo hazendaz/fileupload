dd95ef299b4585187d5ba402e7a36ce532313d7b
Jeremy Landis <jeremylandis@hotmail.com>
2024-05-26 13:05:31 -0400

    [temp] Delete beanProvider until figure out how deltaspike is to be upgraded
    deltaspike deleted this code (or moved it to obsolete packages) with claim this can be done natively but doesn't describe how to actually do it natively.  For now delete this but add it back once figured out as needed.

--- src/main/java/com/hazendaz/ws/HelloWorldImpl.java
+++ src/main/java/com/hazendaz/ws/HelloWorldImpl.java
@@ -13,17 +13,11 @@
  */
 package com.hazendaz.ws;
 
-import com.hazendaz.weld.BeanProvider;
-
-import jakarta.annotation.PostConstruct;
 import jakarta.annotation.Resource;
 import jakarta.enterprise.inject.Vetoed;
 import jakarta.inject.Inject;
 import jakarta.jws.WebService;
 import jakarta.xml.ws.WebServiceContext;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import org.slf4j.Logger;
 
@@ -40,21 +34,11 @@ public class HelloWorldImpl implements HelloWorld {
     @Override()
     public String getHelloWorld(final String name) {
         if (this.logger == null) {
-            this.init();
+            // Will add back later
+            // this.init();
         }
         this.logger.info("I'm not null - logger man");
         return "Hello World JAX-WS " + name;
-    }
-
-    @PostConstruct
-    public void init() {
-        final Map<String, Class<?>> ignoreMap = new HashMap<>();
-        ignoreMap.put("context", Resource.class);
-        try {
-            BeanProvider.injectFields(this, ignoreMap);
-        } catch (final IllegalStateException e) {
-            // Do nothing - Tomcat causes this failure
-        }
     }
 
 }
